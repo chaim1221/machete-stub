6f4b3a7df16c149109ebdf7a2e0160bdf01999fe
Chaim Eliyah <chaimeliyah@gmail.com>
2018-11-21 12:57:24 -0600

    add Data, Domain from original

---
+++ Machete.Data/Repositories/ConfigRepository.cs
@@ -0,0 +1,16 @@
+﻿using Machete.Data.Infrastructure;
+using Machete.Domain;
+
+namespace Machete.Data
+{
+    public interface IConfigRepository : IRepository<Config>
+    {
+    }
+    public class ConfigRepository : RepositoryBase<Config>, IConfigRepository
+    {
+
+        public ConfigRepository(IDatabaseFactory dbFactory) : base(dbFactory)
+        { }
+
+    }
+}
