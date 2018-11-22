23dc507419be88b7049644343760c22afe761e8f
Chaim Eliyah <chaimeliyah@gmail.com>
2018-11-21 21:51:59 -0600

    machete.service stub

---
+++ Machete.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Machete.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
